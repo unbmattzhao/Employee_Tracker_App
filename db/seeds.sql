aaea5969e5035702f18f0b37bd6789adba624fa2
Matt Zhao <mattzhao@MP.ht.home>
2023-04-19 09:29:05 -0300

    seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO departments (name) 
+VALUES
+    ('Sales')
+    ('Human resources'),
+    ('Marketing'),
+    ('Information Technology'),
+    ('Accounting and Finance');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Sales Representative', 8000.00, 1),
+    ('Sales Manager', 10000.00, 1),
+    ('HR Specialist' 7000.00, 2),
+    ('HR Manager', 9000.00, 2),
+    ('Marketing Specialist', 9000.00, 3),
+    ('Marketing Manager', 7500.00, 3),
+    ('IT Engineer', 8500.00, 4),
+    ('IT Manager', 11000.00, 4),
+    ('Financial Specialist', 7000.00, 5),
+    ('Financial Manager', 9000.00, 5);
+
+INSERT INTO employees (first_name, last_name, manager_id)
+VALUES
+('', '', , ,),
